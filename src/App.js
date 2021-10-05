8da8c63ce668922c38bbed566885ddb58112efd4
WAUTERS William <PSR10589@students.ephec.be>
2021-10-05 18:53:43 +0200

    added bootstrap + routing + two new Forms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ControlledForm from './Forms/ControlledForm'
+import UncontrolledForm from './Forms/UncontrolledForm'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Forms With React</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link className="nav-link" to="/Forms/ControlledForm">ControlledForm</Link>
+              </li>
+              <li class="nav-item">
+                <Link className="nav-link" to="/Forms/UncontrolledForm">UncontrolledForm</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Forms/ControlledForm">
+            <ControlledForm />
+          </Route>
+          <Route path="/Forms/UncontrolledForm">
+            <UncontrolledForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Forms/UncontrolledForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+class UncontrolledForm extends React.Component {  
+  render() {  
+    return <h1>UncontrolledForm</h1>  
+  }  
+}  
+export default UncontrolledForm  
