fe52289c76986230748e03f9400d37827b03e80f
WAUTERS William <PSR10589@students.ephec.be>
2021-10-05 19:26:51 +0200

    added basic state management

--- src/Forms/ControlledForm.js
+++ src/Forms/ControlledForm.js
@@ -3,15 +3,36 @@ import React from 'react'
 class ControlledForm extends React.Component {  
   constructor(props) {
     super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+    }
   }
-  
+
+  // Generic way of handling the updating of the state for each onChange
+  // 1. onChange = e => this.setState({ [e.target.name]: e.target.value })
+  // 2. 
+  handleChange(e) {
+    // If you are using babel, you can use ES 6 dictionary syntax
+    // let change = { [e.target.name] = e.target.value }
+    let change = {}
+    change[e.target.name] = e.target.value
+    this.setState(change)
+    console.log(e.target.value)
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {  
     return (
       <div className="container">
           <br />
           <br />
         <div className="row justify-content-center" >
-          <div class="col-8">
+          <div className="col-8">
             <h4>This is a controlled form</h4>
           </div>
           <br />
@@ -19,17 +40,17 @@ class ControlledForm extends React.Component {
           <br />
         </div>
         <div className="row justify-content-center">
-          <div class="col-8">
-            <form>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <div className="col-8">
+            <form onSubmit={this.onSubmit}>
+              <div className="mb-3">
+                <label htmlFor="firstname" className="form-label">Name</label>
+                <input type="text" className="form-control" name="firstname" value={this.state.firstname} onChange={this.handleChange.bind(this)} />
               </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" />
+              <div className="mb-3">
+                <label htmlFor="lastname" className="form-label">Surname</label>
+                <input type="text" className="form-control" name="lastname" value={this.state.lastname} onChange={this.handleChange.bind(this)} />
               </div>
-              <input type="submit" class="btn btn-primary" value="Submit" />
+              <input type="submit" className="btn btn-primary" value="Submit" />
             </form>
           </div>
         </div>
