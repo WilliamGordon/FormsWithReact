bfb2050be6e9622c71795593e5b580c39c43a477
WAUTERS William <PSR10589@students.ephec.be>
2021-10-05 19:11:35 +0200

    added form boiler plate - ControlledForm

--- src/App.js
+++ src/App.js
@@ -13,18 +13,18 @@ function App() {
     <Router>
       <div>
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Forms With React</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Forms With React</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <Link className="nav-link" to="/Forms/ControlledForm">ControlledForm</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/Forms/UncontrolledForm">UncontrolledForm</Link>
               </li>
             </ul>
--- src/Forms/ControlledForm.js
+++ src/Forms/ControlledForm.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
 class ControlledForm extends React.Component {  
+  constructor(props) {
+    super(props);
+  }
+  
   render() {  
-    return <h1>ControlledForm</h1>  
+    return (
+      <div className="container">
+          <br />
+          <br />
+        <div className="row justify-content-center" >
+          <div class="col-8">
+            <h4>This is a controlled form</h4>
+          </div>
+          <br />
+          <br />
+          <br />
+        </div>
+        <div className="row justify-content-center">
+          <div class="col-8">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+              </div>
+              <input type="submit" class="btn btn-primary" value="Submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+    )
   }  
 }  
 export default ControlledForm  
